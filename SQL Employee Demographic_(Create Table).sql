95b245adcdc412e9b02e993b1fa50af59940cb4d
katechanalytic <katechanalytic@gmail.com>
2026-04-05 22:38:43 +0100

    Update SQL Employee Demographic_(Create Table).sql

--- SQL Employee Demographic_(Create Table).sql
+++ SQL Employee Demographic_(Create Table).sql
@@ -2,7 +2,6 @@
 ---CREATE A NEW EMPLOYEE DATABASE
 ---CREATE A TABLE USING NEW QUERY
 
-```
 CREATE TABLE EmployeeDemographics (
 Emp_ID TEXT,
 First_Name TEXT,
@@ -14,9 +13,7 @@ Year_of_Hire INTEGER,
 Years_Service INTEGER,
 Department TEXT
 );
-```
 
-```
 CREATE TABLE EmployeeSalary (
 Emp_ID TEXT,
 First_Name TEXT,
@@ -24,4 +21,3 @@ Last_Name TEXT,
 Annual_Salary INTEGER,
 Pension INTEGER
 );
-```
