4b793abeeb6f0a5dc7183d8367e598017d59453a
katechanalytic <katechanalytic@gmail.com>
2026-04-14 17:18:27 +0100

    Update Module 2 (Creating Table).sql

--- Module 2 (Creating Table).sql
+++ Module 2 (Creating Table).sql
@@ -10,3 +10,9 @@ Year_of_Hire INTEGER,
 Years_Service INTEGER,
 Department TEXT
 );
+
+ --INSERT INTO Employeedemographic VALUES
+ (E1110, 'Barry', 'Uma','M',32,'barry@uma.company.com','20','customer service',2006)
+
+-- INSERT INTO Employee_Salary VALUES
+ (E1110, 'Barry', 'Uma',98000,,6766)
