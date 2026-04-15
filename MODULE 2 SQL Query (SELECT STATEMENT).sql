c595aadfd91eb1548c011388ff3eac55fef8448c
katechanalytic <katechanalytic@gmail.com>
2026-04-15 01:03:27 +0100

    Create MODULE 2 SQL Query (SELECT STATEMENT).sql

---
+++ MODULE 2 SQL Query (SELECT STATEMENT).sql
@@ -0,0 +1,19 @@
+/*
+MODULE 2 SELECT STATEMENT
+*/
+
+SELECT*
+FROM EmployeeDemographics
+
+SELECT*
+FROM Employee_Salary
+
+SELECT First_Name
+FROM EmployeeDemographics
+
+SELECT First_Name, Last_Name
+FROM Employee_Salary
+
+SELECT *
+FROM EmployeeDemographics
+LIMIT 5
