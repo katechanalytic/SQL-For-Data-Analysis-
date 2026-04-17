7ed1a5d481c62e4c9f65377b0e5728fc88845fd8
katechanalytic <katechanalytic@gmail.com>
2026-04-17 18:38:56 +0100

    Update Module 2 SQL Query (UPDATIN & DELETING DATA).sql

--- Module 2 SQL Query (UPDATIN & DELETING DATA).sql
+++ Module 2 SQL Query (UPDATIN & DELETING DATA).sql
@@ -2,25 +2,33 @@
 MODULE 2_UPDATING/DELETING DATA
 */
 
-SELECT*
-FROM EmployeeDemographics
+-- View all records from EmployeeDemographics
+SELECT *
+FROM EmployeeDemographics;
 
-SELECT*
-FROM Employee_Salary es 
+-- View all records from Employee_Salary (aliased as es)
+SELECT *
+FROM Employee_Salary es;
 
+-- Update the Age to 28 for the employee named Bacata Yvette
 UPDATE EmployeeDemographics
 SET Age = 28
-WHERE First_Name = 'Bacata' AND Last_Name = 'Yvette'
+WHERE First_Name = 'Bacata' AND Last_Name = 'Yvette';
 
+-- Update the Gender to 'F' for the employee with ID E1237
 UPDATE EmployeeDemographics
 SET Gender = 'F'
-WHERE Emp_ID = 'E1237'
+WHERE Emp_ID = 'E1237';
 
+-- Delete the record for the employee whose first name is Bacata
 DELETE FROM EmployeeDemographics 
-WHERE First_Name = "Bacata"
+WHERE First_Name = 'Bacata';
 
+-- Select the record for the employee with ID E1235
 SELECT *
 FROM EmployeeDemographics
-WHERE Emp_ID = 'E1235'
+WHERE Emp_ID = 'E1235';
 
-DROP TABLE IF EXISTS Temp_employee_Database
+-- Drop the temporary table Temp_employee_Database if it exists
+DROP TABLE IF EXISTS Temp_employee_Database;
+
