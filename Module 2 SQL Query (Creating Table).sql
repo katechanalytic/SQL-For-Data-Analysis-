74ef79179120283d174a8c2195e02bda1be5ad88
katechanalytic <katechanalytic@gmail.com>
2026-04-17 18:29:29 +0100

    Update Module 2 SQL Query (Creating Table).sql

--- Module 2 SQL Query (Creating Table).sql
+++ Module 2 SQL Query (Creating Table).sql
@@ -1,4 +1,6 @@
 ----MODULE 2 CREATING TABLE 
+
+--Creating a table to store employee demographic information
 CREATE TABLE EmployeeDemographics (
 Emp_ID TEXT,
 First_Name TEXT,
@@ -11,9 +13,10 @@ Years_Service INTEGER,
 Department TEXT
 );
 
-----INSERTING VALUES INTO A TABLE
+--Inserting values into the EmployeeDemographics table
  INSERT INTO Employeedemographic VALUES
  (E1110, 'Barry', 'Uma','M',32,'barry@uma.company.com','20','customer service',2006)
 
+ --Inserting values into the Employee_Salary table
 INSERT INTO Employee_Salary VALUES
  (E1110, 'Barry', 'Uma',98000,6766)
