5cde90c81f876cd3e25788e1178608242eb6eb23
katechanalytic <katechanalytic@gmail.com>
2026-04-19 23:00:46 +0100

    Update Module 4 SQL Query (UNION & UNION ALL).sql

--- Module 4 SQL Query (UNION & UNION ALL).sql
+++ Module 4 SQL Query (UNION & UNION ALL).sql
@@ -2,37 +2,42 @@
 MODULE 4 UNION, UNION ALL
 */
 
-SELECT*
-FROM EmployeeDemographics ed 
+-- View all records from EmployeeDemographics
+SELECT *
+FROM EmployeeDemographics ed;
 
-SELECT*
-FROM Employee_Salary es 
+-- View all records from Employee_Salary
+SELECT *
+FROM Employee_Salary es;
 
-SELECT*
+-- Perform a full outer join between demographics and salary
+-- This returns all rows from both tables, matching on Emp_ID
+SELECT *
 FROM EmployeeDemographics ed 
 FULL OUTER JOIN Employee_Salary es 
-ON ed.Emp_ID =es.Emp_ID 
+ON ed.Emp_ID = es.Emp_ID;
 
-SELECT*
+-- UNION combines results from two queries and removes duplicates
+-- Since both queries are identical, the result will just be the same as one query
+SELECT *
 FROM Employee_Salary es 
 UNION 
 SELECT * 
-FROM Employee_Salary es 
+FROM Employee_Salary es;
 
-SELECT*
+-- UNION ALL combines results from two queries and keeps duplicates
+-- Since both queries are identical, every row will appear twice
+SELECT *
 FROM Employee_Salary es 
 UNION ALL 
 SELECT * 
-FROM Employee_Salary es 
+FROM Employee_Salary es;
 
-SELECT*
+-- UNION ALL again, but with ORDER BY applied to the final result
+-- This will show all rows (including duplicates) sorted by Emp_ID
+SELECT *
 FROM Employee_Salary es 
 UNION ALL 
 SELECT * 
 FROM Employee_Salary es
-ORDER BY Emp_ID
-
-
-
-
-
+ORDER BY Emp_ID;
