4fa7219a568c366aeadce0344ce6668eef6e728b
katechanalytic <katechanalytic@gmail.com>
2026-04-19 22:31:46 +0100

    Update Module 4 SQL Query (Working with Multiple Table {JOINS}).sql

--- Module 4 SQL Query (Working with Multiple Table {JOINS}).sql
+++ Module 4 SQL Query (Working with Multiple Table {JOINS}).sql
@@ -57,13 +57,23 @@ ON es.Emp_ID = ed.Emp_ID
 WHERE ed.First_Name = "Bacata"
 ORDER BY Annual_Salary DESC 
  
- 
 SELECT ed.Department, Avg(es.Annual_Salary)
 FROM EmployeeDemographics ed 
 INNER JOIN  Employee_Salary es 
 ON es.Emp_ID = ed.Emp_ID 
 WHERE Department = "Human Resources"
 ORDER BY Annual_Salary DESC 
+
+--Which states had cheese production greater than 100 million in April 2023? 
+--The Cheese Department wants to focus their marketing efforts there. 
+--How many states are there?
+SELECT L.State,Period,YEAR,Value ,L.State_ANSI
+FROM cheese_production P
+INNER JOIN state_lookup L
+ON P.State_ANSI = L.State_ANSI
+WHERE P.Value <> 100000000
+ AND P.Period = 'APR'
+ AND P.Year = 2023;
 
 
 
