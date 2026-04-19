262d410f02ff8ed35bc2760cfc78a5efc9667c64
katechanalytic <katechanalytic@gmail.com>
2026-04-19 23:25:57 +0100

    Update Module 5 SQL Query (Data Cleaning).sql

--- Module 5 SQL Query (Data Cleaning).sql
+++ Module 5 SQL Query (Data Cleaning).sql
@@ -2,46 +2,54 @@
 MODULE 5 DATA CLEANING {STRING FUNCTION} - (TRIM, LTRIM, RTRIM, REPLACE, SUBSTRING, LOWER, UPPER)
 */
 
-CREATE TABLE Employee_table
-(EmployeeID varchar (50),
-first_Name varchar(50),
-Last_Name varchar(50)
-)
+-- Create a simple table with messy string data
+CREATE TABLE Employee_table (
+    EmployeeID VARCHAR(50),
+    First_Name VARCHAR(50),
+    Last_Name VARCHAR(50)
+);
 
-SELECT *
-FROM Employee_table
-
-INSERT INTO  Employee_table VALUES
+-- Insert sample records with extra spaces and inconsistent formatting
+INSERT INTO Employee_table VALUES
 ('E246 ', ' Jimo ', 'Farad  '),
 (' E289', ' Austin', '  Beckley '),
-('  E265', ' Tomi ', ' Fenades -   Hamza')
+('  E265', ' Tomi ', ' Fenades -   Hamza');
 
-----USING TRIM, LTRIM, RTRIM
+-- View the raw data
+SELECT *
+FROM Employee_table;
 
+---- USING TRIM, LTRIM, RTRIM
+-- Remove spaces from both ends
 SELECT EmployeeID, TRIM(EmployeeID) AS IDTRIM
-FROM  Employee_table
+FROM Employee_table;
 
+-- Remove spaces from the right side only
 SELECT EmployeeID, RTRIM(EmployeeID) AS IDTRIM
-FROM  Employee_table
+FROM Employee_table;
 
+-- Remove spaces from the left side only
 SELECT EmployeeID, LTRIM(EmployeeID) AS IDTRIM
-FROM  Employee_table
+FROM Employee_table;
 
-----USING REPLACE
+---- USING REPLACE
+-- Clean up Last_Name by removing unwanted substring
+SELECT Last_Name, REPLACE(Last_Name, ' Fenades -', '') AS Last_Name_Cleaned 
+FROM Employee_table;
 
-SELECT Last_Name, REPLACE(Last_Name, ' Fenades -','') AS Last_Name_Cleaned 
-FROM Employee_table
+---- USING SUBSTRING
+-- Extract the first 4 characters of First_Name
+SELECT SUBSTRING(First_Name, 1, 4)
+FROM Employee_table;
 
-----USING SUBTRING
+---- USING UPPER AND LOWER
+-- Convert names to lowercase
+SELECT First_Name, LOWER(First_Name)
+FROM Employee_table;
 
-SELECT SUBSTRING (first_Name,1,4)
-FROM Employee_table
+-- Convert names to uppercase
+SELECT First_Name, UPPER(First_Name)
+FROM Employee_table;
 
----- USING UPPER AMD LOWER
-SELECT first_Name, LOWER(first_Name)
-FROM Employee_table
-
-SELECT first_Name, UPPER(first_Name)
-FROM Employee_table
-
-DROP TABLE Employee_table
+-- Drop the table after testing
+DROP TABLE Employee_table;
