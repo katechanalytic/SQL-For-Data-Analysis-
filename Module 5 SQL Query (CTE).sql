21bc7dbc79242284afa17d082c6a671e05ccb5c9
katechanalytic <katechanalytic@gmail.com>
2026-04-19 23:21:34 +0100

    Update Module 5 SQL Query (CTE).sql

--- Module 5 SQL Query (CTE).sql
+++ Module 5 SQL Query (CTE).sql
@@ -1,26 +1,30 @@
 /*
-MODULE 5 CTE (COMMON TABLE EXPRESSION)
+ MODULE 5 CTE (COMMON TABLE EXPRESSION)
 */
 
-WITH CTE_EMPLOYEE AS 
-(SELECT DEMO.First_Name, DEMO.Last_Name, DEMO.Department,
-COUNT (DEMO.Department ) OVER (PARTITION BY DEMO.Department) AS Total_Employee,
-AVG (Annual_Salary) OVER (PARTITION BY Annual_Salary) AS Average_Salary
-FROM EmployeeDemographics AS DEMO
-JOIN Employee_Salary AS SAL
-ON DEMO.Emp_ID =SAL.Emp_ID
-WHERE Annual_Salary > '80000')
+-- Build a CTE to calculate employee counts and average salary
+WITH CTE_EMPLOYEE AS (
+    SELECT DEMO.First_Name, DEMO.Last_Name, DEMO.Department,
+           COUNT(DEMO.Department) OVER (PARTITION BY DEMO.Department) AS Total_Employee,
+           AVG(SAL.Annual_Salary) OVER (PARTITION BY DEMO.Department) AS Average_Salary
+    FROM EmployeeDemographics AS DEMO
+    JOIN Employee_Salary AS SAL
+    ON DEMO.Emp_ID = SAL.Emp_ID
+    WHERE SAL.Annual_Salary > 80000
+)
 SELECT *
-FROM CTE_EMPLOYEE
+FROM CTE_EMPLOYEE;
 
-WITH CTE_EMPLOYEE AS 
-(SELECT DEMO.First_Name, DEMO.Last_Name, DEMO.Department,
-COUNT (DEMO.Department ) OVER (PARTITION BY DEMO.Department) AS Total_Employee,
-AVG (Annual_Salary) OVER (PARTITION BY SAL.Annual_Salary) AS Average_Salary
-FROM EmployeeDemographics AS DEMO
-JOIN Employee_Salary AS SAL
-ON DEMO.Emp_ID =SAL.Emp_ID
-WHERE Annual_Salary > '80000')
-SELECT First_Name, Department,Average_Salary 
-FROM CTE_EMPLOYEE
 
+-- CTE, but selecting only specific columns
+WITH CTE_EMPLOYEE AS (
+    SELECT DEMO.First_Name, DEMO.Last_Name, DEMO.Department,
+           COUNT(DEMO.Department) OVER (PARTITION BY DEMO.Department) AS Total_Employee,
+           AVG(SAL.Annual_Salary) OVER (PARTITION BY DEMO.Department) AS Average_Salary
+    FROM EmployeeDemographics AS DEMO
+    JOIN Employee_Salary AS SAL
+    ON DEMO.Emp_ID = SAL.Emp_ID
+    WHERE SAL.Annual_Salary > 80000
+)
+SELECT First_Name, Department, Average_Salary 
+FROM CTE_EMPLOYEE;
