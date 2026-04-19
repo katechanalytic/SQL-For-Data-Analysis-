d9b609605d7c103f6b77006eb54cacee10cd52aa
katechanalytic <katechanalytic@gmail.com>
2026-04-19 22:05:47 +0100

    Update Module 3 SQL Query (Aggregating Data).sql

--- Module 3 SQL Query (Aggregating Data).sql
+++ Module 3 SQL Query (Aggregating Data).sql
@@ -2,6 +2,7 @@
  COUNT, SUM, MAX, MIN, AVG
 */
 
+-----EMPLOYEE DEMOGRAPHIC DATABASE
 -- Count how many employees have a non-null Last_Name
 SELECT COUNT(Last_Name)
 FROM EmployeeDemographics;
@@ -21,5 +22,21 @@ FROM Employee_Salary;
 -- Calculate the total sum of all annual salaries
 SELECT SUM(Annual_Salary)
 FROM Employee_Salary;
+
+----PROUCTION TABLE (DATABASE)
+--Find the Min yogurt production for the year 2021.
+SELECT MIN(Value)
+FROM yogurt_production yp 
+WHERE Year = '2021';
+
+--Find the average honey production for the year 2022.
+SELECT AVG(Value)
+FROM honey_production
+WHERE Year = '2022';
+
+--Find the Max egg production for the year 2023.
+SELECT MAX(Value)
+FROM egg_production ep 
+WHERE Year = '2023';
 
 
